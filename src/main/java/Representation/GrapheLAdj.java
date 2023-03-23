a6c1e3552e2aabe1a7f7d08bc288955678d4bbd2
Owen <rebellerowen@gmail.com>
2023-03-23 20:35:30 +0100

    source2

--- src/main/java/Representation/GrapheLAdj.java
+++ src/main/java/Representation/GrapheLAdj.java
@@ -1,14 +1,17 @@
 package Representation;
 
 import Sommet_Arc.Arc;
+import Sommet_Arc.Sommet;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GrapheLAdj {
     private Map<String, List<Arc>> ladj;
 
-    public GrapheLAdj() {
-        ;
+    public GrapheLAdj(Sommet sommet) {
+        ladj = new HashMap<>();
+
     }
 }
--- src/main/java/Representation/GrapheMAdj.java
+++ src/main/java/Representation/GrapheMAdj.java
@@ -7,7 +7,18 @@ public class GrapheMAdj {
     private int[][] matrice;
     private Map<String, Integer> indice;
 
-    public GrapheMAdj(int sommet) {
-        matrice = new int[sommet][sommet];
+    public GrapheMAdj(int nbrSommet) {
+        matrice = new int[nbrSommet][nbrSommet];
+        initMatrice(nbrSommet);
     }
+
+    private void initMatrice(int nbrSommet) {
+        assert (nbrSommet == 0);
+        for (int i = 0; i < nbrSommet; i++) {
+            for (int j = 0; j < nbrSommet; j++) {
+                matrice[i][j] = 0;
+            }
+        }
+    }
+    
 }
